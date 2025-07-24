ac4b44f4109069a069a38d759cdde1703d044dc4
Matthew McLaughlin <matthedm@uci.edu>
2025-07-23 17:51:06 -0700

    Basic version

---
+++ Cat Press/Cat Press/Cat_PressApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_PressApp.swift
+//  Cat Press
+//
+//  Created by Matthew McLaughlin on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct Cat_PressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
